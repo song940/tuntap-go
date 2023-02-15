28f4c0d48c64f3f8a13f42f6f36bb2d39ed5b39d
Lsong <song940@gmail.com>
2023-02-15 19:08:15 +0800

    update

--- cli/cli.go
+++ cli/cli.go
@@ -3,7 +3,6 @@ package cli
 import (
 	"log"
 
-	"github.com/song940/tuntap-go/packet"
 	"github.com/song940/tuntap-go/tuntap"
 )
 
@@ -11,23 +10,17 @@ func Run() {
 	config := tuntap.Config{
 		DeviceType: tuntap.TUN,
 	}
-	config.Name = "utun9"
 	ifce, err := tuntap.New(config)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("Interface Name: %s\n", ifce.Name())
-	var packet packet.Packet
-	packet.Resize(1500)
+	buf := make([]byte, 1500)
 	for {
-		n, err := ifce.Read(packet)
+		n, err := ifce.Read(buf)
 		if err != nil {
 			log.Fatal(err)
 		}
-		packet.Resize(n)
-		header, _ := packet.ParseHeader()
-		log.Println(header.Version)
-		log.Println(header.Src)
-		log.Println(header.Dst)
+		log.Printf("Received packet from TUN device: %v", buf[:n])
 	}
 }
--- up.sh
+++
@@ -1,3 +0,0 @@
-
-# sudo ip addr add 10.1.0.10/24 dev utun9
-# sudo ip link set dev utun9 up
